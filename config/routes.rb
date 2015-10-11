eaa3ec464c6243582699286c481349262cf9ee10
Alexis <alexistran@berkeley.edu>
2015-10-11 06:39:21 +0000

    I'm done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   get "users/new", to: "users#new"
   get "users/:id", to: "users#show", as: :user
   post "users", to: "users#create"
+
+  get "dogs", to: "dogs#index"
+  #get "dogs/:id", to: "dogs#show", as: :user
+  get "dogs/new", to: "dogs#new"
+  post "dogs", to: "dogs#create"
 end
